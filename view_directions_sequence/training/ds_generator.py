c1a04e2c7a7a74f3a8c88325091044440bb34a0b
MockaWolke <82398122+MockaWolke@users.noreply.github.com>
2022-08-19 16:53:59 +0800

    Seq_DS

---
+++ view_directions_sequence/training/ds_generator.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+def get_df():
+    df = pd.read_feather("training.df").set_index("index")
+
+    train_df = df.loc[df["ds_type"]=="train"]
+    val_df = df.loc[df["ds_type"]=="val"]
+    test_df = df.loc[df["ds_type"]=="test"]
+
+    return train_df,val_df,test_df
+
+
+def write_cloud_data_frame():
+    
+    df = pd.read_feather("training.df").set_index("index")
+
+    path_to_train = {}
+
+    for paths, kind in df.iloc[:,[-4,-1]].values:
+
+        for p in paths:
+            path_to_train[p] = kind
+
+    paths = np.unique(np.concatenate(df["Paths"].values))
+    paths = pd.DataFrame(paths,columns=["Path"])
+    paths["ds_type"] = paths.Path.map(path_to_train)
+    paths["key"] = paths.Path.apply(lambda s: s[s.find("images/")+7:-4])
+    paths.to_feather("paths_ds.feather")
+
+def mapping(img1,img2,img3,img4,img5,label):
+
+    img1 = tf.io.decode_jpeg(tf.io.read_file(img1), channels=3)
+    img2 = tf.io.decode_jpeg(tf.io.read_file(img2), channels=3)
+    img3 = tf.io.decode_jpeg(tf.io.read_file(img3), channels=3)
+    img4 = tf.io.decode_jpeg(tf.io.read_file(img4), channels=3)
+    img5 = tf.io.decode_jpeg(tf.io.read_file(img5), channels=3)
+
+    img_seq = tf.stack([img1,img2,img3,img4,img5])
+
+    label = tf.one_hot(label,2)
+
+    return img_seq, label
+
+
+def get_ds(df,batch_size, kind = "train",colab = False): 
+    assert kind in ["train","test","val"]
+
+    paths = np.array([i for i in df.Paths.apply(np.array).values])
+
+    if colab:
+        func = lambda s: s[s.find("images/")+7:]
+        paths = np.array([[func(s) for s in row]  for row in paths])
+    else:
+        paths = "../../" + paths
+        
+    labels = (df.view_direction == "Sideways").apply(int).values
+
+    ds = tf.data.Dataset.from_tensor_slices((paths[:,0],paths[:,1],paths[:,2],paths[:,3],paths[:,4],labels))
+    ds = ds.map(mapping)
+    ds = ds.batch(batch_size=batch_size, drop_remainder= kind!='test')
+    ds = ds.prefetch(tf.data.AUTOTUNE)
+
+    return ds
+
+
+def get_train_and_val(batch_size = 64,colab = False):
+
+    train_df,val_df,test_df = get_df()
+
+    train_ds = get_ds(train_df,batch_size=batch_size,kind="train",colab = colab)
+    val_ds = get_ds(val_df,batch_size = batch_size,kind="val",colab = colab)
+
+    return train_ds,val_ds
+
+def get_test_ds(batch_size = 64,colab = False):
+
+    train_df,val_df,test_df = get_df()
+
+    test_ds = get_ds(test_df,batch_size=batch_size,kind="test",colab = colab)
+
+    return test_ds
+
+
